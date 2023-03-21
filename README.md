19580e90d350c060a644827e0118d2a1bf083c49
jiyuujin <jiyuujinunite@gmail.com>
2023-03-21 20:01:06 +0900

    initialize

---
+++ README.md
@@ -0,0 +1,15 @@
+# OpenAI (Whisper)
+
+For detailed instructions, please refer to the [documentation](https://www.serverless.com/framework/docs/providers/aws/).
+
+## Install
+
+Run `npm i` to install the project dependencies.
+
+### Use Local
+
+`npx sls invoke local -f hello` if you're using npm.
+
+### Deploy Lambda
+
+Run `npx sls deploy` to deploy this stack to AWS Lambda.
---
+++ handler.ts
@@ -0,0 +1,79 @@
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import * as FormData from 'form-data'
+import fetch from 'node-fetch'
+
+const OPENAPI_AUDIO_TRANSCRIPTIONS_API = 'https://api.openai.com/v1/audio/transcriptions'
+const OPENAPI_SECRET = ''
+
+export const audioTranscriptions = async (file, object): Promise<string> => {
+  const body = new FormData()
+  body.append('model', 'whisper-1')
+  body.append('file', file, {
+    contentType: object.ContentType,
+    knownLength: object.ContentLength,
+    filename: file['Key'],
+  })
+
+  const res = await fetch(OPENAPI_AUDIO_TRANSCRIPTIONS_API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      Authorization: `Bearer ${OPENAPI_SECRET}`,
+    },
+    body,
+  })
+  const data = await res.json()
+
+  return data.text
+};
+
+export const hello: APIGatewayProxyHandler = async (event) => {
+  const reqBody = event['Records'][0]
+  const key = decodeURIComponent(reqBody.s3.object.key.replace(/\+/g, ' '))
+
+  const s3 = new AWS.S3({
+    region:'ap-northeast-1',
+  })
+
+  const s3Params = {
+    Bucket: 'openai-whisper-bucket',
+    ACL: 'public-read',
+  }
+
+  let title = ''
+
+  try {
+    s3.headObject({ ...s3Params, Key: key }, (err, output) => {
+      if (err) return
+
+      const s3Stream = s3.getObject({ ...s3Params, Key: key })
+        .createReadStream()
+
+      const res = audioTranscriptions(s3Stream, output)
+      title = res.content
+    })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          text: title,
+        },
+        null,
+        2,
+      ),
+    }
+  } catch (err) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify(
+        {
+          text: err || 'Something is wrong.',
+        },
+        null,
+        2,
+      ),
+    }
+  }
+}
