2901b0e2bcdaf5b4788fd17253dbdd73a01f9cc8
jiyuujin <jiyuujinunite@gmail.com>
2023-05-06 17:41:43 +0900

    feat: transcribe

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,21 +2,50 @@ import { createSignal } from 'solid-js'
 import logo from './logo.svg'
 import './App.css'
 
+const OPENAI_AUDIO_TRANSCRIPTIONS_API = 'https://api.openai.com/v1/audio/transcriptions'
+const OPENAI_SECRET = ''
+
+// Model: whisper-1
+const OPENAI_MODEL = 'whisper-1'
+
+const LAMBDA_AUDIO_TRANSCRIPTIONS_API = ''
+
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [formData, setFormData] = createSignal<FormData>()
+  const [answer, setAnswer] = createSignal('')
+
+  function onChange(e) {
+    const data = new FormData()
+    data.append('file', e.target.files[0])
+    data.append('model', OPENAI_MODEL)
+    setFormData(data)
+  }
+
+  async function onTranscribe() {
+    const response = await fetch(OPENAI_AUDIO_TRANSCRIPTIONS_API, {
+      method: 'post',
+      headers: {
+        Authorization: `Bearer ${OPENAI_SECRET}`,
+      },
+      body: formData(),
+    })
+
+    const data = await response.json()
+    const { text } = data
+    setAnswer(text)
+  }
 
   return (
     <div class="App">
       <header class="App-header">
         <img src={logo} class="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello Vite + SolidJS + OpenAI!</p>
+        <div>
+          <input type="file" name="file1" onChange={onChange}/>
+          <button onClick={onTranscribe}>送信する</button>
+        </div>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count()}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
+          Answer: <code>{answer()}</code>
         </p>
         <p>
           <a
